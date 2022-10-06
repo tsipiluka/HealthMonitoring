82970b47f3978074fe027575d6279b943f686311
DWsRekt <thedavidlord@yahoo.de>
2022-10-06 20:08:00 +0200

    Added imports for Api

--- HealthMonitoring/HealthDashboard/views.py
+++ HealthMonitoring/HealthDashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from HealthDashboard.models import Person, Patient, Arzt, Befund, PatientenZugriff, ArztZugriff
+from HealthDashboard.serializers import PersonSerializer, PatientSerializer, ArztSerializer, BefundSerializer, PatientenZugriffSerializer, ArztZugriffSerializer 
 
 # Create your views here.
+
