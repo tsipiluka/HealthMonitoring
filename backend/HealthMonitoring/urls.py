790b6f92569389b11b1a200c644cf1a58a65450c
Moritz <moritz.rfs@gmail.com>
2022-10-06 23:42:17 +0200

    Fix deprecated url path

--- backend/HealthMonitoring/urls.py
+++ backend/HealthMonitoring/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, re_path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path(r'^', include('HealthDashboard.urls'))
 ]
