24a3ec40fa3df9c95d08ec1570723091fbf059f7
DWsRekt <thedavidlord@yahoo.de>
2022-11-15 18:24:26 +0100

    Created Django Backend with Authentication and Api-Interfaces

---
+++ healthmonitoring/medicalFinding/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedicalFindingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "medicalFinding"
---
+++ healthmonitoring/medicalFinding/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from medicalFinding.models import MedicalFinding
+
+class MedicalFindingSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MedicalFinding
+        exclude = ['created_at', 'updated_at']
+    
