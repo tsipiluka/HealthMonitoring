90124e1723657d1852eebb84730ac710e7533c98
DWsRekt <thedavidlord@yahoo.de>
2022-11-15 22:42:01 +0100

    Created dashboard overview

--- frontend/healthmonitoring/src/app/app.module.ts
+++ frontend/healthmonitoring/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
+    DashboardComponent,
     
   ],
   imports: [
