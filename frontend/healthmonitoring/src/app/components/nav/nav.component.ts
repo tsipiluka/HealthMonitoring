0edce5090411fcdee51786f40624ce84e0bf0dd9
DWsRekt <thedavidlord@yahoo.de>
2022-11-24 17:00:52 +0100

    Added Navbar Component

---
+++ frontend/healthmonitoring/src/app/components/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor( private router: Router) {
+
+  }
+
+  ngOnInit() {
+    
+  } 
+
+  logout(){
+    localStorage.clear()
+    this.router.navigate(['login'])
+  }
+}
