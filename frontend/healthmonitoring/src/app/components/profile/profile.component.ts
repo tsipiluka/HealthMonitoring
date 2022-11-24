5da93b9880193fcf1c1163ac07f99c61f4f31621
DWsRekt <thedavidlord@yahoo.de>
2022-11-24 22:16:39 +0100

    Changed Profile Component

--- frontend/healthmonitoring/src/app/components/profile/profile.component.ts
+++ frontend/healthmonitoring/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from './service/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  birthday: Date | undefined
+
+  passwordChangeModel: boolean = false
+
+  old_password: string | undefined
+  new_password1: string | undefined
+  new_password2: string | undefined
+
+  constructor(
+    private router: Router,
+    private profileService: ProfileService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  displayPasswordChangeModel(){
+    this.passwordChangeModel = true
+  }
+
+  resetPassword(){
+    this.passwordChangeModel = false
+    if(this.checkPassword(this.new_password1!) && this.checkPassword(this.new_password2!)){
+      const passwords = {
+        'old_password': this.old_password,
+        'new_password': this.new_password1,
+        'new_password2': this.new_password2
+      }
+      this.profileService.resetPassword(passwords).subscribe(()=>{
+        localStorage.clear()
+        this.router.navigate(['login'])
+      })
+    }
+  }
+
+  checkPassword(password: string): boolean{
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[^\w\s])(?=.{8,})/.test(password)
+  }
 }
