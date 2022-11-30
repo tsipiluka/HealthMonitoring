24941072cccf3fe47b1b422cd7affc5c1997a948
DWsRekt <thedavidlord@yahoo.de>
2022-11-30 16:42:13 +0100

    Fixed login from username to email with validation

--- frontend/healthmonitoring/src/app/components/login/login.component.ts
+++ frontend/healthmonitoring/src/app/components/login/login.component.ts
@@ -30,9 +30,8 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    if(this.email !== '' && this.password !== ''){
-    // if(this.checkEmail(this.email) && this.password !== '' && this.captchaStatus){
-      const creds = {username: this.email, password: this.password}
+    if(this.validateEmail(this.email) && this.validatePassword(this.password) && this.captchaStatus){
+      const creds = {email: this.email, password: this.password}
       this.loginService.loginUser(creds).subscribe((res:any)=>{
         localStorage.setItem('access_token', res.access)
         localStorage.setItem('refresh_token', res.refresh)
@@ -44,8 +43,12 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  checkEmail(email: string): boolean{
+  validateEmail(email: string): boolean{
     return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+  }
+
+  validatePassword(password: string): boolean{
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[^\w\s])(?=.{8,})/.test(password)
   }
 
   captchaSuccess(event: any){
