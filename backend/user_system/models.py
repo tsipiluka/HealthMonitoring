0405c104582ddc7845076111e310661630fbed74
moritzrfs <moritz.rfs@gmail.com>
2022-11-30 11:28:56 +0100

    Add UserManager

--- backend/user_system/models.py
+++ backend/user_system/models.py
@@ -3,7 +3,36 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 class PatientManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
@@ -22,6 +51,15 @@ class User(AbstractUser):
         DOCTOR = "DOCTOR", "Doctor"
 
     base_role = Role.PATIENT
+
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
     role = models.CharField(max_length=50, choices=Role.choices, default=base_role)
     birth_date = models.DateField(null=True, blank=True)
