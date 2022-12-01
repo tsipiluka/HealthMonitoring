9e93a0a46b8703bf8da79b6b97ea21ca2b725242
DWsRekt <thedavidlord@yahoo.de>
2022-12-01 21:07:39 +0100

    Fixed Service for Dashbaord

--- frontend/healthmonitoring/src/app/components/dashboard/service/dashboard.service.ts
+++ frontend/healthmonitoring/src/app/components/dashboard/service/dashboard.service.ts
@@ -14,18 +14,18 @@ export class DashboardService {
   loadMedicalFindings(): Observable<any>{
     const headers= new HttpHeaders()
     .set('Authorization', 'Bearer '+localStorage.getItem('access_token'));
-    return this.http.get(this.APIUrl + '/api/medicalFinding/' ,{'headers': headers})
+    return this.http.get(this.APIUrl + '/api/medical_findings/' ,{'headers': headers})
   }
   
   createMedicalFinding(medicalFinding_info: any): Observable<any>{
     const headers= new HttpHeaders()
     .set('Authorization', 'Bearer '+localStorage.getItem('access_token'));
-    return this.http.post(this.APIUrl + '/api/medicalFinding/' ,medicalFinding_info, {'headers': headers})
+    return this.http.post(this.APIUrl + '/api/medical_finding/' ,medicalFinding_info, {'headers': headers})
   }
 
   deleteMedicalFinding(uid: any): Observable<any>{
     const headers= new HttpHeaders()
     .set('Authorization', 'Bearer '+localStorage.getItem('access_token'));
-    return this.http.delete(this.APIUrl + '/api/medicalFinding/',{'body': uid, 'headers': headers})
+    return this.http.delete(this.APIUrl + '/api/medical_finding/',{'body': uid, 'headers': headers})
   }
 }
