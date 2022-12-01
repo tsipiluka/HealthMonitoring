7d06e57ffd36698711ea697aff5a91c15b39671a
Moritz <moritz.rfs@gmail.com>
2022-12-01 12:23:32 +0100

    Fix formatting

--- backend/user_system/seralizers.py
+++ backend/user_system/seralizers.py
@@ -2,33 +2,61 @@ from rest_framework import serializers
 from user_system.models import PatientProfile, DoctorProfile
 from user_system.models import Doctor, Patient, User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        
-        fields = ('id', 'email', 'first_name', 'last_name', 'birth_date', 'is_doctor', 'is_patient', 'is_email_verified')
+
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "birth_date",
+            "is_doctor",
+            "is_patient",
+            "is_email_verified",
+        )
         # make first_name and last_name required
         extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
         }
-        
+
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('id', 'email', 'first_name', 'last_name', 'birth_date', 'is_doctor', 'is_patient')
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "birth_date",
+            "is_doctor",
+            "is_patient",
+        )
+
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = ('id', 'email', 'first_name', 'last_name', 'birth_date')
+        fields = ("id", "email", "first_name", "last_name", "birth_date")
+
 
 class PatientProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = PatientProfile
-        fields = ('patient_id',)
+        fields = ("patient_id",)
+
 
 class DoctorProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorProfile
-        fields = ('doctor_id',)
+        fields = ("doctor_id",)
+
+
+class LightUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
