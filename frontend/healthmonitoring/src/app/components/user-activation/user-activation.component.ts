f52cd2e59168c10698b49f0fafca7f48b623769a
DWsRekt <thedavidlord@yahoo.de>
2022-12-01 14:25:34 +0100

    Finished user Activation

--- frontend/healthmonitoring/src/app/components/user-activation/user-activation.component.ts
+++ frontend/healthmonitoring/src/app/components/user-activation/user-activation.component.ts
@@ -11,16 +11,11 @@ export class UserActivationComponent implements OnInit{
 
   constructor(private userActivationService: UserActivationService,private router: Router,private route: ActivatedRoute){}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   activateAccount(){
     this.route.params.subscribe(params => {
       this.userActivationService.activateUser(params['token'], params['uidb64']+'==').subscribe((res: any)=>{
-        // localStorage.setItem('access_token', res.access)
-        // localStorage.setItem('refresh_token', res.refresh)
-        // this.router.navigate(['dashboard'])
         this.router.navigate(['login'])
       })
     })
