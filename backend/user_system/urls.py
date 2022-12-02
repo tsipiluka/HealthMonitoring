caaf13644088a08b2c534d4fae412e1da6ff97f0
Moritz <moritz.rfs@gmail.com>
2022-12-02 14:15:20 +0100

    Add Endpoints

--- backend/user_system/urls.py
+++ backend/user_system/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = "user_system"
 urlpatterns = [
     path("user_profile/", views.UserProfileView.as_view(), name="user_profile"),
+    path("user/", views.GetUserByProfiledID.as_view(), name="user_profile"),
 ]
