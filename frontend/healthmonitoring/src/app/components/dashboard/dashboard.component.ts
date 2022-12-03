9323d09b5dcaa2724bd32ea9e479805c2a71c6f4
DWsRekt <thedavidlord@yahoo.de>
2022-12-03 13:08:20 +0100

    neue changes

--- frontend/healthmonitoring/src/app/components/dashboard/dashboard.component.ts
+++ frontend/healthmonitoring/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MedicalFinding } from 'src/app/entities/medicalFinding.modal';
 import { LoginService } from '../login/service/login.service';
 import { DashboardService } from './service/dashboard.service';
 import { jsPDF } from "jspdf";
 import { UserService } from 'src/app/services/user-service/user.service';
+
+export interface ReadAccessObject{
+  medical_finding: string,
+  reader: number,
+  uid: string
+}
+
+export interface ReadAccessUser{
+  [key: string]: any
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -16,12 +26,17 @@ export class DashboardComponent implements OnInit {
   medicalFindingList: MedicalFinding[] = []
   user: any 
 
-  addEntryModel: boolean = false
+  modify_mode: boolean = false
+  medicalFindingModel: boolean = false
+  selectedMedicalFinding: MedicalFinding | undefined
+
+  tester: boolean = true
 
   new_disease: string | undefined
   new_medicine: string | undefined
   selectedDoctorID: string | undefined
   selectedUsers: string[] = []
+  currentReadAccessObjects: ReadAccessUser = {}
 
   constructor(private userService: UserService,private loginService: LoginService,private dashboardService: DashboardService,  private router: Router) {}
 
@@ -30,7 +45,6 @@ export class DashboardComponent implements OnInit {
       "refresh": localStorage.getItem('refresh_token')
     } 
     this.loginService.refreshToken(refresh_token).subscribe((res: any) => {
-      console.log(res)
       localStorage.setItem('access_token', res.access)
       this.userService.getUserInformation().subscribe((userInfo: any)=>{
         this.user = userInfo
@@ -74,13 +88,39 @@ export class DashboardComponent implements OnInit {
   }
 
   displayAddEntryModel(){
-    this.addEntryModel = true
+    this.modify_mode = false
+    this.medicalFindingModel = true
+  }
+
+  displayChangeEntryModel(medicalFinding: MedicalFinding){
+    this.selectedMedicalFinding = medicalFinding
+    this.selectedUsers = [];
+    this.new_disease = medicalFinding.disease
+    this.new_medicine = medicalFinding.medicine
+    this.selectedDoctorID = medicalFinding.treator !== null ? medicalFinding.treator.doctor_profile.doctor_id : ''
+    this.dashboardService.getReadAccessFromMedicalFinding(medicalFinding.uid).subscribe((user: any)=>{
+      for(let i = 0; i < user.length; i++){
+        if(user[i]!.reader.role ==='PATIENT'){
+          this.currentReadAccessObjects[String(user[i]!.reader.patient_profile.patient_id)] = user[i]
+          this.selectedUsers.push(String(user[i]!.reader.patient_profile.patient_id))
+        }else{
+          this.currentReadAccessObjects[String(user[i]!.reader.patient_profile.patient_id)] = user[i]
+          this.selectedUsers.push(String(user[i]!.reader.doctor_profile.doctor_id))
+        }
+      }
+      this.selectedUsers = [...this.selectedUsers]
+      this.modify_mode = true
+      this.medicalFindingModel = true
+    })
+  }
+
+  getModifyEntryModelHeader(){
+    return this.modify_mode ? "Bitte nehmen Sie ihre Änderungen vor" : "Füge einen neuen Eintrag hinzu"
   }
 
   createNewMedicalFinding(){
     if(this.new_disease !== '' && this.new_medicine !== ''){
-      console.log(this.selectedDoctorID)
-      if(this.selectedDoctorID === undefined){
+      if(this.selectedDoctorID === undefined || this.selectedDoctorID === '' || this.selectedDoctorID === null){
         const medicalFinding_info = { 
           'disease': this.new_disease, 
           'medicine': this.new_medicine,
@@ -102,23 +142,47 @@ export class DashboardComponent implements OnInit {
 
   createNewMedicalFindingHelper(medicalFinding_info: any){
     this.dashboardService.createMedicalFinding(medicalFinding_info).subscribe((medicalFinding: any)=>{
-      for(let i = 0; i<this.selectedUsers!.length; i++){
+      for(let i = 0; i<this.selectedUsers.length; i++){
         const profil_id = {profile_id: this.selectedUsers[i]}  
         this.userService.getUserId(profil_id).subscribe((user2: any) => {
           const readUser = {reader: user2.id}
-          this.dashboardService.addReadAccessToMedicalFinding(medicalFinding.uid, readUser).subscribe((res: any)=> {
-            console.log(res)
-          })
+          this.dashboardService.addReadAccessToMedicalFinding(medicalFinding.uid, readUser).subscribe()
         })
       }
-      console.log("hi")
-      this.addEntryModel = false
+      this.medicalFindingModel = false
       this.new_disease = ''
       this.new_medicine = ''
       this.selectedDoctorID = ''
       this.selectedUsers = []
       this.loadMedicalFindings()
     })
+  }
+
+  getKeys(list: any): string[]{
+    return Object.keys(list)
+  }
+
+  changeMedicalFinding(){
+    const a = 1
+  }
+
+  deleteReadAccess(key: string){
+    if(this.modify_mode){
+      const profil_id = {profile_id: key}  
+      this.dashboardService.deleteReadAccessFromMedicalFinding(this.selectedMedicalFinding!.uid, this.currentReadAccessObjects[key].reader.id).subscribe()
+    }
+  }
+
+  addReadAccess(key:string){
+    if(this.modify_mode){
+      const profil_id = {profile_id: key}  
+      this.userService.getUserId(profil_id).subscribe((user: any) => {
+        const readUser = {reader: user.id}
+        this.dashboardService.addReadAccessToMedicalFinding(this.selectedMedicalFinding!.uid, readUser).subscribe((res: any)=>{
+          this.currentReadAccessObjects[key] = res
+        })
+      })
+    }
   }
 
   checkIfDoctor(){
