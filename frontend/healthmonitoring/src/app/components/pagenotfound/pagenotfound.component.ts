8532cf11df1cec34ce02d88c264b48d5280c3711
DWsRekt <thedavidlord@yahoo.de>
2022-12-04 21:49:09 +0100

    Created PageNotFound Component

---
+++ frontend/healthmonitoring/src/app/components/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+  styleUrls: ['./pagenotfound.component.css']
+})
+export class PagenotfoundComponent {
+  constructor(private router: Router){}
+
+  redirectToDashboard(){
+    this.router.navigate(['dashboard'])
+  }
+}
