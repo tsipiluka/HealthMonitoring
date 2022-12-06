baa397fce418dd9257c518f4b9150306533e1765
Moritz <moritz.rfs@gmail.com>
2022-12-06 16:09:24 +0100

    Add Download and Delete method

--- backend/upload_files/models.py
+++ backend/upload_files/models.py
@@ -5,7 +5,6 @@ from medical_finding.models import MedicalFinding
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
-    file_name = models.CharField(max_length=100, blank=False, null=False)
     medical_finding = models.ForeignKey(MedicalFinding, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
