8303b0bd506e8369c695d13c3cbc93bdd5c20ca3
DWsRekt <thedavidlord@yahoo.de>
2022-12-06 17:37:06 +0100

    Fixed Authorization for Download Request

--- backend/download_files/views.py
+++ backend/download_files/views.py
@@ -38,36 +38,26 @@ class FileDownloadView(APIView):
         Returns a decrypted file from the server
         if the file is not found, returns a 404
         """
-        # # str to uuid
-        # if not is_valid_uuid(medical_finding_id):
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        print("Medical finding id: " + medical_finding_id)
-        # remove "" from string
-        medical_finding_id = medical_finding_id[1:-1]
-
-        # medical_finding = MedicalFinding.objects.get(pk=medical_finding_id)
-        #print("Medical finding: " + medical_finding)
-
         
-        # if not medical_finding:
-        #     print("Medical finding is none")
-        #     return Response(status=status.HTTP_404_NOT_FOUND)
+        if not is_valid_uuid(medical_finding_id):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        # if not (request.user == medical_finding.patient or request.user == medical_finding.treator):
-        #     if not FindingReadingRight.objects.filter(medical_finding=medical_finding, reader=request.user).exists():
-        #         return Response(status=status.HTTP_403_FORBIDDEN)
+        medical_finding = MedicalFinding.objects.get(pk=medical_finding_id)
 
+        if not medical_finding:
+            print("Medical finding is none")
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
+        if not (request.user == medical_finding.patient or request.user == medical_finding.treator):
+            if not FindingReadingRight.objects.filter(medical_finding=medical_finding, reader=request.user).exists():
+                return Response(status=status.HTTP_403_FORBIDDEN)
 
-
-        # f = File.objects.filter(medical_finding=medical_finding_id).first()
-        # if not f:
-        #     return Response(status=status.HTTP_404_NOT_FOUND)
-        # try:
-        #     ef = EncryptedFile(f.file)
-        # except:
-        #     return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        f = File.objects.filter(medical_finding=medical_finding_id).first()
+        if not f:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            ef = EncryptedFile(f.file)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-        # return HttpResponse(ef.read(), content_type=get_media_type(f.file.name))
-        return Response(status=status.HTTP_200_OK)
+        return HttpResponse(ef.read(), content_type=get_media_type(f.file.name))
