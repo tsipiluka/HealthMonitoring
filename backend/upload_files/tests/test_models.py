163519171f367d97343a5f962c3f95b36ed52c28
Moritz <moritz.rfs@gmail.com>
2022-12-07 11:29:37 +0100

    Add download_file testcases

--- backend/upload_files/tests/test_models.py
+++ backend/upload_files/tests/test_models.py
@@ -12,15 +12,20 @@ class TestFileModel(TestCase):
             finding = TestMedicalFinding().test_create_medical_finding(patient=patient)
         else:
             finding = TestMedicalFinding().test_create_medical_finding()
+        
+        if doctor is not None:
+            finding.treator = doctor
+            finding.save()
+
         file_path = os.path.join(os.path.dirname(__file__), "upload_files", "test.txt")
         file_data = SimpleUploadedFile(
             file_path, b"file_content", content_type="text/plain"
         )
 
-        print(file_path)
         file = File.objects.create(
             file=file_data,
             medical_finding=finding,
         )
-
+        
         self.assertEqual(file.medical_finding, finding)
+        return file
