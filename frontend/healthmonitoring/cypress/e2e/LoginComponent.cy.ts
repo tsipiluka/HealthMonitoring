c1ed1feabebd3fef727fa3ac0c4b99edceae8be0
DWsRekt <thedavidlord@yahoo.de>
2022-12-07 23:45:41 +0100

    Modified Login Component Test for Pipeline

--- frontend/healthmonitoring/cypress/e2e/LoginComponent.cy.ts
+++ frontend/healthmonitoring/cypress/e2e/LoginComponent.cy.ts
@@ -7,17 +7,19 @@ describe('Login Testing', () => {
   const valid_pass = 'Tester123321+'
   const correct_valid_email = 'pat1@pat.de'
 
-  beforeEach(()=>{
-    cy.visit('http://localhost:4200/login')
-  })
+  // beforeEach(()=>{
+  //   cy.visit('http://localhost:4200/login')
+  // })
 
   it('try to login without any credentials', () => {
+    cy.visit('http://localhost:4200/login')
     cy.get('#loginBtn').click()
     cy.get('#toastMessage').should('contain.text', 'Bitte tragen Sie eine gültige Email ein!')
     cy.url().should('include', 'login')
   })
 
   it('try to login only with unvalid email type', () => {
+    cy.visit('http://localhost:4200/login')
     cy.get('#loginEmail').type('false-email.de')
     cy.get('#loginBtn').click()
     cy.get('#toastMessage').should('contain.text', 'Bitte tragen Sie eine gültige Email ein!')
@@ -25,6 +27,7 @@ describe('Login Testing', () => {
   })
 
   it('try to login only with valid email type', () => {
+    cy.visit('http://localhost:4200/login')
     cy.get('#loginEmail').type(email)
     cy.get('#loginBtn').click()
     cy.get('#toastMessage').should('contain.text', 'Das Password entspricht nicht den Anforderungen!')
@@ -32,6 +35,7 @@ describe('Login Testing', () => {
   })
 
   it('try to login with valid email type and unvalid password type', () => {
+    cy.visit('http://localhost:4200/login')
     cy.get('#loginEmail').type(email)
     cy.get('#loginPassword').type(unvalid_pass)
     cy.get('#loginBtn').click()
@@ -40,6 +44,7 @@ describe('Login Testing', () => {
   })
 
   it('correct login for patient', () => {
+    cy.visit('http://localhost:4200/login')
     cy.get('#loginEmail').type(correct_valid_email)
     cy.get('#loginPassword').type(valid_pass)
     cy.get('#captchaElem').click()
