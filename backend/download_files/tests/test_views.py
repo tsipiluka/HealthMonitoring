48e2789cc4dde4396d0363c5b78444453e339413
Moritz <moritz.rfs@gmail.com>
2022-12-07 11:29:37 +0100

    Add download_file testcases

--- backend/download_files/tests/test_views.py
+++ backend/download_files/tests/test_views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from user_system.tests.test_models import TestUserModel
 from medical_finding.tests.test_models import TestMedicalFinding
+from upload_files.models import File
+from upload_files.tests.test_models import TestFileModel
 
 
 class TestFileDownload(APITestCase):
@@ -16,57 +18,80 @@ class TestFileDownload(APITestCase):
         """
         user = TestUserModel().test_patient()
         refresh = RefreshToken.for_user(user)
-        medical_finding = TestMedicalFinding().test_create_medical_finding(patient=user)
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
-        file_path = os.path.join(os.path.dirname(__file__), "upload_files", "test.txt")
-        # with file open
-        with open(file_path, "rb") as file:
-            response = self.client.post(
-                reverse("upload_file"),
-                {"file": file,
-                 "medical_finding": medical_finding.uid
-                 },
-
-                format="multipart",
-            )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response = self.client.get(
-            reverse("download_file", kwargs={"uid": medical_finding.uid})
-        )
+        file = TestFileModel().test_create_file(patient=user)
+        medical_finding = file.medical_finding.uid
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': medical_finding}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_download_file_no_finding(self):
+    
+    def test_download_file_other_patient(self):
         """
-        Test if a file can be downloaded without a medical finding.
-        Expected: 400 BAD REQUEST
+        Test if a file can be downloaded by another patient.
+        Expected: 403 Forbidden
+        """
+        user = TestUserModel().test_patient(email="patient1@test.de")
+        refresh = RefreshToken.for_user(user)
+        file = TestFileModel().test_create_file()
+        medical_finding = file.medical_finding.uid
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': medical_finding}))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_download_file_doctor(self):
+        """
+        Test if a file can be downloaded by a doctor if he 
+        is assigned as the treating doctor in the medical finding
+        which is the parent object of the file.
+        Expected: 200 OK
+        """
+        doctor = TestUserModel().test_doctor(email="doctor1@test.de")
+        refresh = RefreshToken.for_user(doctor)
+        file = TestFileModel().test_create_file(doctor=doctor)
+        medical_finding = file.medical_finding.uid
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': medical_finding}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_download_file_not_assigned_doctor(self):
+        """
+        Test if a file can be downloaded by a doctor if he 
+        is not assigned as the treating doctor in the medical finding
+        which is the parent object of the file.
+        Expected: 403 Forbidden
+        """
+        doctor = TestUserModel().test_doctor(email="doctor1@test.de")
+        refresh = RefreshToken.for_user(doctor)
+        file = TestFileModel().test_create_file()
+        medical_finding = file.medical_finding.uid
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': medical_finding}))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_download_invalid_medical_finding_id(self):
+        """
+        Test if a file can be downloaded with an invalid medical finding id.
+        Expected: 400 Bad Request
         """
         user = TestUserModel().test_patient()
         refresh = RefreshToken.for_user(user)
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
-        file_path = os.path.join(os.path.dirname(__file__), "upload_files", "test.txt")
-        # with file open
-        with open(file_path, "rb") as file:
-            response = self.client.post(
-                reverse("upload_file"),
-                {"file": file
-                 },
-
-                format="multipart",
-            )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        response = self.client.get(
-            reverse("download_file", kwargs={"uid": "invalid"})
-        )
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': "invalid"}))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_download_file_invalid_finding(self):
+    def test_download_valid_random_medical_finding_id(self):
         """
-        Test if a file can be downloaded with an invalid medical finding.
-        Expected: 400 BAD REQUEST
+        Test if a file can be downloaded with a valid but random
+        medical finding id which doesnt exist in the database.
+        Expected: 400 Bad Request
         """
         user = TestUserModel().test_patient()
         refresh = RefreshToken.for_user(user)
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
-        file_path = os.path.join(os.path.dirname(__file__), "upload_files", "test.txt")
-        # with file open
-        with open(file_path
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse('download-file', kwargs={'medical_finding_id': "00000000-0000-0000-0000-000000000000"}))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
