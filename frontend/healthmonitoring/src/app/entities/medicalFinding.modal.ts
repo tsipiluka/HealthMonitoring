a04133b1c614b2c895cdf7f0e7871647a518d5c3
DWsRekt <thedavidlord@yahoo.de>
2022-12-07 13:42:51 +0100

    Changed medicine to comment in medicalfinding modal

--- frontend/healthmonitoring/src/app/entities/medicalFinding.modal.ts
+++ frontend/healthmonitoring/src/app/entities/medicalFinding.modal.ts
@@ -4,7 +4,7 @@ import { Patient } from "./patient.modal";
 export interface IMedicalFinding {
     uid: string,
     disease: string,
-    medicine: string,
+    comment: string,
     updated_at: Date,
     patient: Patient,
     treator: Doctor,
@@ -15,7 +15,7 @@ export class MedicalFinding implements IMedicalFinding {
     constructor(
         public uid: string,
         public disease: string,
-        public medicine: string,
+        public comment: string,
         public updated_at: Date,
         public patient: Patient,
         public treator: Doctor,
