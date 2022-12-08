0a08de2725ee0ed0a939c002c12423b3cc513d35
DWsRekt <thedavidlord@yahoo.de>
2022-12-08 16:08:13 +0100

    Added Validation Service for email and password

---
+++ frontend/healthmonitoring/src/app/services/validateInput-service/validate-input-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidateInputService {
+
+  constructor() { }
+
+  validateEmail(email: string): boolean{
+    return /^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$/.test(email)
+  }
+
+  validatePassword(password: string): boolean{
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[^\w\s])(?=.{8,})/.test(password)
+  }
+}
