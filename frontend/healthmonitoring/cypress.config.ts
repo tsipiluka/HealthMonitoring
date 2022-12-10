e3b63252e9f6ac54f11ec6b71793b6c75582d987
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-12-10 22:05:37 +0100

    latest try

--- frontend/healthmonitoring/cypress.config.ts
+++ frontend/healthmonitoring/cypress.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: "mzjqaj",
+  projectId: 'mzjqaj',
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:4200'
+    baseUrl: 'http://localhost:80',
   },
 });
