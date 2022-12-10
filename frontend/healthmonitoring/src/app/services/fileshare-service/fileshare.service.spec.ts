3b12085264e2099e991cce769e19e1aec43ebce3
DWsRekt <thedavidlord@yahoo.de>
2022-12-10 18:06:17 +0100

    Fixed Fileshare test fail

--- frontend/healthmonitoring/src/app/services/fileshare-service/fileshare.service.spec.ts
+++ frontend/healthmonitoring/src/app/services/fileshare-service/fileshare.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { FileshareService } from './fileshare.service';
@@ -6,7 +7,9 @@ describe('FileshareService', () => {
   let service: FileshareService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(FileshareService);
   });
 
