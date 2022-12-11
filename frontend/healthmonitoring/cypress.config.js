de7a28dc7289608ec4840091fafb3aeb5663667d
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-12-11 15:13:38 +0100

    latest not workin try

--- frontend/healthmonitoring/cypress.config.js
+++ frontend/healthmonitoring/cypress.config.js
@@ -1,16 +1,16 @@
-module.exports = require('cypress').defineConfig({
-  component: {
-    reporter: 'junit',
-    reporterOptions: {
-      mochaFile: 'cypress/results/output.xml',
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/results/output.xml',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
     },
-    e2e: {
-      // We've imported your old cypress plugins here.
-      // You may want to clean this up later by importing these.
-      setupNodeEvents(on, config) {
-        return require('./cypress/plugins/index.js')(on, config);
-      },
-      baseUrl: 'http://localhost:4200',
-    },
-  }
+    baseUrl: 'http://localhost:8080',
+  },
 });
