a9d3ef51354fa0590376560f6d63ce695bff5172
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-12-11 15:23:26 +0100

    quick changes

---
+++ frontend/healthmonitoring/cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: 'mzjqaj',
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts',
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
