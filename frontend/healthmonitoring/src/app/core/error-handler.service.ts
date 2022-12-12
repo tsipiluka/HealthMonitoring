1a9171e0850ed2d250b5038074a59d4125e3f02f
DWsRekt <thedavidlord@yahoo.de>
2022-12-12 20:41:49 +0100

    Fixed Error handler

--- frontend/healthmonitoring/src/app/core/error-handler.service.ts
+++ frontend/healthmonitoring/src/app/core/error-handler.service.ts
@@ -15,18 +15,6 @@ export class ErrorHandlerService {
         localStorage.clear();
         this.router.navigate(['login']);
         break
-      case 400: // 401 - Unauthorized
-        localStorage.clear();
-        this.router.navigate(['login']);
-        break
-      // case 404: // 404 - Not Found - URL nicht gefunden
-      //   this.router.navigate(['login']);
-      //   break
-      // case 403: // 403 - Forbidden - keine Berechtigungen
-      //   if((new RegExp('/shoppinglist/[1-9]*')).test(redirectUrl!)){
-      //     this.router.navigate(['list-overview'])
-      //   }
-      //   break
     }
   }
 }
